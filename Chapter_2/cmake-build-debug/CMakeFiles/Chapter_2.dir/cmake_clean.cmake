d8291b0d373314fa2298b12f2b795f4cfee88a90
toddl <Toddlabo@gmail.com>
2019-05-20 10:16:14 -0600

    Adding Cmake

---
+++ Chapter_2/cmake-build-debug/CMakeFiles/Chapter_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter_2.dir/main.cpp.o"
+  "Chapter_2.pdb"
+  "Chapter_2.exe"
+  "libChapter_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chapter_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Chapter_2/cmake-build-debug/CMakeFiles/ch.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ch.dir/main.cpp.o"
+  "ch.pdb"
+  "ch.exe"
+  "libch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
