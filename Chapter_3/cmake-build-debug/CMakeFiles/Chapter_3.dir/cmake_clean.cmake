fcade4af0318525367a6bda29027f15dd1ad444a
toddl <Toddlabo@gmail.com>
2019-05-20 10:21:02 -0600

    Adding initial Files for Chapter_3

---
+++ Chapter_3/cmake-build-debug/CMakeFiles/Chapter_3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter_3.dir/main.cpp.o"
+  "Chapter_3.pdb"
+  "Chapter_3.exe"
+  "libChapter_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chapter_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Chapter_3/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Chapter 3!" << std::endl;
+    return 0;
+}
