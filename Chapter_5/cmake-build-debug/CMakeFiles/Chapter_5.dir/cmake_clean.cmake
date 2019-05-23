f20a302ea1145b8163384bff480debaa38420c7a
toddl <Toddlabo@gmail.com>
2019-05-23 08:19:12 -0600

    Added Chapter 5 folder

---
+++ Chapter_5/cmake-build-debug/CMakeFiles/Chapter_5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter_5.dir/main.cpp.o"
+  "Chapter_5.pdb"
+  "Chapter_5.exe"
+  "libChapter_5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chapter_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
